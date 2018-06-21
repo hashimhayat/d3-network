2c57e50e92377facdaf8275b580dc55087d7b9d6
Hashim Hayat <hashimhayat@Hashims-MacBook-Pro.local>
2018-06-21 18:11:14 +0500

    changes with bg

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Network V2.0' });
 });
 
+/* GET home page. */
+router.get('/black', function(req, res, next) {
+  res.render('black', { title: 'Network V2.0' });
+});
+
+
 module.exports = router;
